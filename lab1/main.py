354765d84e0edbbb87b1ce9c99eb83c19a46c6ad
S-vchslvoo <qsccnfc123@gmail.coom>
2025-03-01 09:30:57 +0300

    fix: lab1

--- lab1/main.py
+++ lab1/main.py
@@ -3,24 +3,29 @@ def find_the_max(arr):
     arr_list = {}
     for index in range(len(arr)):
         try:
-            difference = arr[index+1]-arr[index]
+            difference = abs(arr[index+1])-abs(arr[index])
         except:
             continue
-        arr_list[index] = difference
+        if difference <0:
+            arr_list[index] = abs(difference)
+        else:
+            arr_list[index+1] = difference
+
     return arr_list
 
 
 
 def main():
     arr = list(map(int, input('Введите последовательность: ').split()))
-    res_list = find_the_max(arr)
+    sorted_arr = sorted(arr)    
+    res_list = find_the_max(sorted_arr)
     max_key = max(res_list, key=res_list.get)
     
     if res_list[max_key] == 1:
-        print(*arr)
+        print(*sorted_arr)
         return
     
-    print(arr[max_key])
+    print(sorted_arr[max_key])
 
 
 if __name__ == "__main__":
