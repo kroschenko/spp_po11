1cb386e73f6f8b7aef0ed90e032a51b0ef00ac44
ILGurin <126956586+ILGurin@users.noreply.github.com>
2025-04-05 10:12:29 +0300

    fix: some changes

--- reports/Gurin/4/src/api.py
+++ reports/Gurin/4/src/api.py
@@ -7,7 +7,9 @@ def get_github_data(username: str) -> str:
     headers = {"Authorization": f"Bearer {MainConst.GITHUB_TOKEN}"}
     query: str = load_query_from_file(MainConst.QUERY_FILENAME)
     variables = {"username": username}
-    response = post(MainConst.GITHUB_API_GRAPHQL_ENDPOINT, headers=headers, json={"query": query, "variables": variables})
+    response = post(
+        MainConst.GITHUB_API_GRAPHQL_ENDPOINT, headers=headers, json={"query": query, "variables": variables}
+    )
     return response.json()
 
 
