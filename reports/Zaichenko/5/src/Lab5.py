7f3313fd82ee98296f97529411ca880715910353
Cтанислав <qsccnfc123@gmail.com>
2025-04-26 02:09:41 +0300

    fix: lab5

--- reports/Zaichenko/5/src/Lab5.py
+++ reports/Zaichenko/5/src/Lab5.py
@@ -701,6 +701,6 @@ def delete_grade(grade_id: int, db: SessionLocal = Depends(get_db)):
 
 
 if __name__ == "__main__":
-
     print("Запуск FastAPI приложения...")
     uvicorn.run(app, host="0.0.0.0", port=8000)
+    
